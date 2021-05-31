f413b441e8fe2c48bbecf5beffe517c306b50862
GonzaloRo <chalo543@hotmail.com>
2021-05-31 10:25:42 -0600

    first commit

---
+++ app/src/main/java/arduino/sistemamonitorizacion/MainActivity.java
@@ -0,0 +1,14 @@
+package arduino.sistemamonitorizacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
